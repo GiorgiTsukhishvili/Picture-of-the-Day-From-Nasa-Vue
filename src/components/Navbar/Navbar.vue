a84f794c18e6d6607350e34a9f1d8c01261672c6
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-04-30 15:40:20 +0400

    feat: add styles to navbar

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-    <RouterLink :to="{ name: 'favorites' }"> Favorites </RouterLink>
+  <div
+    class="bg-gray-100 h-14 flex justify-center items-center gap-5 shadow-lg"
+  >
+    <RouterLink
+      :to="{ name: 'home' }"
+      class="font-bold hover:scale-125 duration-75 hover:text-purple-700 uppercase"
+      >Home</RouterLink
+    >
+    <span class="bg-black rounded-full h-2 w-2"></span>
+    <RouterLink
+      :to="{ name: 'favorites' }"
+      class="font-bold hover:scale-125 duration-75 hover:text-purple-700 uppercase"
+    >
+      Favorites
+    </RouterLink>
   </div>
 </template>
 
