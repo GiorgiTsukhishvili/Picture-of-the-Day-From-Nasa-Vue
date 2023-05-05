34c7698c628961b627f8eafc62653bb8b8d63d68
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-05-05 15:06:38 +0400

    feat: add ability to fetch data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,20 @@
   <div></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { fetchNasaImages } from "@/services";
+import { onMounted, ref } from "vue";
+
+const nasaImages = ref<string>([]);
+
+onMounted(() => {
+  const images = async () => {
+    const data = await fetchNasaImages(10);
+    nasaImages.value = data.data;
+  };
+
+  images();
+});
+</script>
 
 <style scoped></style>
