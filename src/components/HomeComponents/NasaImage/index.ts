90c45d4f55eeb7ea1f0bfb56ae1bacec63b3383e
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-05-06 15:14:42 +0400

    feat: add nasa images component

---
+++ src/components/HomeComponents/NasaImage/index.ts
@@ -0,0 +1,1 @@
+export { default as NasaImage } from "./NasaImage.vue";
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <template>
-  <div></div>
+  <div class="flex flex-col items-center mt-16 gap-10">
+    <NasaImage v-for="(image, i) in nasaImages" :key="i" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { fetchNasaImages } from "@/services";
 import { onMounted, ref } from "vue";
 import type { NasaImagesTypes } from "@/types";
+import { NasaImage } from "@/components";
 
 const nasaImages = ref<NasaImagesTypes[]>([]);
 
